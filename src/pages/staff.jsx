240254c156235b542d65792821eda14c8b488df4
Thilan Dissanayaka <43875064+Thil4n@users.noreply.github.com>
2022-12-24 00:01:00 +0530

    init

---
+++ src/pages/staff.jsx
@@ -0,0 +1,167 @@
+import React, { useState } from "react";
+import { Button, Input } from "../Components/common/ui";
+import { toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+import Axios from "axios";
+
+const Admin = () => {
+    const input_data_structure = {
+        name: {
+            key: "name",
+            text: "Name",
+            type: "text",
+            value: "",
+            is_valid: true,
+            error: "",
+        },
+        email: {
+            key: "email",
+            text: "Email",
+            type: "email",
+            value: "",
+            is_valid: true,
+            error: "",
+        },
+        nic: {
+            key: "nic",
+            text: "nic",
+            type: "text",
+            value: "",
+            is_valid: true,
+            error: "",
+        },
+        password: {
+            key: "password",
+            text: "Password",
+            type: "password",
+            value: "",
+            mask: true,
+            is_valid: true,
+            error: "",
+        },
+        password_conf: {
+            key: "password_conf",
+            text: "Password confirmation",
+            type: "password",
+            value: "",
+            mask: true,
+            is_valid: true,
+            error: "",
+        },
+    };
+
+    const [inputs, set_inputs] = useState(input_data_structure);
+    const [title, set_title] = useState("citizen");
+
+    const handle_change = (e, input) => {
+        input.value = e.target.value;
+        input.is_valid = e.target.value ? true : false;
+        input.error = e.target.value ? "" : "Please input the password";
+
+        let input_list = { ...inputs };
+        input_list[input.key] = input;
+        set_inputs(input_list);
+    };
+
+    const handle_submit = async () => {
+        const event = window.event;
+        event.preventDefault();
+        const data = {
+            name: inputs.name.value,
+            email: inputs.email.value,
+            nic: inputs.nic.value,
+            password: inputs.password.value,
+            password_conf: inputs.password_conf.value,
+        };
+
+        try {
+            Axios.post("127.0.0.1/api/Signup/", data)
+                .then((response) => {
+                    if (response.status == 200) {
+                        const user_data = {
+                            token: response.data.result.token,
+                        };
+
+                        toast.success("Registered successfully!");
+                    }
+                })
+                .catch((e) => {
+                    toast.error(e);
+                });
+        } catch (e) {
+            toast.error(e);
+        }
+    };
+
+    const show_tab = (tab) => {
+        set_title(tab);
+    };
+
+    return (
+        <>
+            <ToastContainer />
+            <div className="grid grid-cols-12 bg-[#0234E2] min-h-[100vh]">
+                <div className="col-span-2">
+                    <div
+                        className="grid grid-cols-3 hover:bg-[#0256E2] cursor-pointer"
+                        onClick={() => show_tab("citizen")}
+                    >
+                        <img
+                            className="w-[70px] h-auto m-auto mt-[15px]"
+                            src="/assets/Rectangle36.png"
+                        ></img>
+                        <div className="col-span-2">
+                            <h1 className="text-white mt-8 ">Citizens</h1>
+                        </div>
+                    </div>
+                    <div
+                        className="grid grid-cols-3 hover:bg-[#0256E2] cursor-pointer"
+                        onClick={() => show_tab("admin")}
+                    >
+                        <img
+                            className="w-[70px] h-auto m-auto mt-[15px]"
+                            src="/assets/Rectangle36.png"
+                        ></img>
+                        <div className="col-span-2">
+                            <h1 className="text-white mt-8 ">Admins</h1>
+                        </div>
+                    </div>
+                    <div
+                        className="grid grid-cols-3 hover:bg-[#0256E2] cursor-pointer"
+                        onClick={() => show_tab("staff")}
+                    >
+                        <img
+                            className="w-[70px] h-auto m-auto mt-[15px]"
+                            src="/assets/Rectangle36.png"
+                        ></img>
+                        <div className="col-span-2">
+                            <h1 className="text-white mt-8 ">Staff</h1>
+                        </div>
+                    </div>
+                    <div
+                        className="grid grid-cols-3 hover:bg-[#0256E2] cursor-pointer"
+                        onClick={() => show_tab("programms")}
+                    >
+                        <img
+                            className="w-[70px] h-auto m-auto mt-[15px]"
+                            src="/assets/Rectangle36.png"
+                        ></img>
+                        <div className="col-span-2">
+                            <h1 className="text-white mt-8 ">Programms</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-span-10 bg-[#0131B7] rounded-l-[50px]">
+                    <div className="mt-[50px] ml-20 w-[80%]">
+                        <h1 className="text-white font-extrabold text-3xl">
+                            {title}
+                        </h1>
+                        <div className="grid "></div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Admin;
